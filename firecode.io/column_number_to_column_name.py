6f1a04a8e08043dea03be420d8db120e47442ffc
Jacob Ville <VilleLJacob@gmail.com>
2016-11-08 15:53:09 -0500

    Add to firecode: Excel spreadsheet column number to column name (base change)

---
+++ firecode.io/column_number_to_column_name.py
@@ -0,0 +1,22 @@
+def excel_column_number_to_name(column_number):
+    # Approach: Convert the same way binary is converted to decimal
+    # Divide the number by the base (26 because of the alphabet)
+    # Get the number from the remainder (Using ascii characters, A starts at 65)
+
+    name = ""
+
+    # Continue until the length of the alphabet no longer "fits" in the number
+    while column_number > 0:
+        # Perform quotient and remainder operations
+        column_number, remainder = divmod(column_number, 26)
+        # Edge case when remainder is 0, append Z
+        if remainder == 0:
+            name += 'Z'
+            # Decrement column number because a value is taken away as 'Z'
+            column_number -= 1
+        else:
+            # Add the character to the output
+            name += chr(remainder + 64)
+
+    # return output in reverse, because it is calculated from lsb to msb
+    return name[::-1]
--- firecode.io/test.py
+++ firecode.io/test.py
@@ -1,24 +1,11 @@
+# def excel_column_number_to_name(column_number):
+#     column_name = ""
+#
+#     while column_number / 26 > 0:
+#
+#
+#     return column_name[::-1]
 
-
-from_name = "Jacob"
-from_address_1 = "Addr 1"
-from_address_2 = "Addr 2"
-from_city = "City"
-from_state = "State"
-from_zip = "ZIP"
-
-from_address = collections.OrderedDict()
-
-from_address['name'] = from_name
-from_address['address_line1'] = from_address_1
-from_address['address_line2'] = from_address_2
-from_address['address_city'] = from_city
-from_address['address_state'] = from_state
-from_address['address_zip'] = from_zip
-
-address_string = ""
-
-for name, info in from_address.items():
-    address_string += info
-
-print address_string
+# print excel_column_number_to_name(26)
+print 52/26
+print (52/26)/26
